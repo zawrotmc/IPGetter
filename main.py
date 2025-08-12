f06ace5c3d0285d3da4159308bdee3b436cac8a5
zawrotmc <ulmanekkacperek@gmail.com>
2025-08-12 21:03:37 +0200

    All Changes

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
